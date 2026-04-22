b5846e7c611d1d5a7a5c4b47d85946895a650dac
yefercacaiz28-beep <yefercacaiz28@gmail.com>
2026-04-21 23:42:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@Like Tutorial Bot"]
+REQUIRED_CHANNELS = ["@liketutoriabot"]
 GROUP_JOIN_LINK = "https://t.me/liketutoriabot"
 OWNER_ID = 5045169905
 OWNER_USERNAME = "@Yeferlikefree"
