44982b28791bb9304528652a0183b619299fa22a
yefercacaiz28-beep <yefercacaiz28@gmail.com>
2026-04-21 23:13:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@your channel username"]
-GROUP_JOIN_LINK = "https://t.me/your_group_link"
-OWNER_ID = your tg user id (integer)    #Example: 6282811167
-OWNER_USERNAME = "@your username"
+REQUIRED_CHANNELS = ["@liketutorial002"]
+GROUP_JOIN_LINK = "https://t.me/liketutorialgroupf"
+OWNER_ID = 5045169905
+OWNER_USERNAME = "@Yeferlikefree"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 like_tracker = {}   # in-memory cache
